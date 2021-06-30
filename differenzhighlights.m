134f799d2e526209eb737226bb41c1b664e1b8c0
taoi666 <81561314+taoi666@users.noreply.github.com>
2021-06-30 22:58:39 +0200

    Add files via upload

--- differenzhighlights.m
+++ differenzhighlights.m
@@ -1,68 +1,116 @@
 clear;
-load('colormap.mat')
-load('colormaplow.mat')
-load('colormapverylow.mat')
-addpath('dataset/Columbia Glacier')
-filePattern = fullfile('dataset/Columbia Glacier', '*.jpg');%%Brazilian Rainforest,Columbia Glacier,Dubai,Kuwait
+% load('colormap.mat')
+% load('colormaplow.mat')
+% load('colormapverylow.mat')
+addpath('dataset/Wiesn')
+filePattern = fullfile('dataset/Wiesn', '*.jpg');%%Brazilian Rainforest, Columbia Glacier, Dubai, Kuwait，Wiesn
 imagefiles = dir(filePattern);
+
+w=fir1(40,0.5);
+kernel = w'*w;
+
+%% select images
+a=8;
+b=6;
+
+%% load data 
 for i=1:length(imagefiles)
     currentfilename = imagefiles(i).name;
-    image{i}= imread(currentfilename);
+    img_ori{i}=imread(currentfilename);
 end
-%%
-%%grayscal of two images, problem bei e.g 6&7
-imgA=image{1};
-%figure;imshow(imgA)
-grayA=rgb2gray(imgA);
-%X = grayslice(grayA,255);
-%m = double(max(X(:)));
-%figure;imshow(X,jet(m))
 
-imgB=image{3};
-%figure;imshow(imgB)
+figure;montage({img_ori{a},img_ori{b}})
+%% Preprocessing
+for i=1:length(img_ori)
+    %%Enhance contrast using histogram equalization
+    image{i}= histeq(img_ori{i});
+    %%Adjust histogram of 2-D image to match histogram of reference image
+    image{i}= imhistmatch(image{i},image{1});
+    %%prefilter
+    image{i} = prefilterlowpass2d(double(image{i}), kernel);
+    image{i} = uint8(image{i});
+    %%cut google
+    image{i} = image{i}(1:end-50,:,:);
+end
+
+%% Registration
+%%problem:6&8
+imgA=image{a};
+imgB=image{b};
+figure;montage({imgA,imgB})
+%figure;imshow(image{1})
+% 
+% grayA=rgb2gray(imgA);
+% grayA=grayA(1:end-50,:,:);
+% grayB=rgb2gray(imgB);
+% figure;montage({grayA,grayB})
+
+grayA=rgb2gray(imgA);
 grayB=rgb2gray(imgB);
+grayB=imhistmatch(grayB,grayA);
+figure;montage({grayA,grayB})
+
 %%SURF
-pointsA=detectSURFFeatures(grayA);
+inlierIdx=[];
+n=0;
+%MetricThreshold=3000;
+%MetricThreshold=MetricThreshold-500;
+pointsA=detectSURFFeatures(grayA,'MetricThreshold',500);
 [featuresA,pointsA]=extractFeatures(grayA,pointsA);
-pointsB=detectSURFFeatures(grayB);
+pointsB=detectSURFFeatures(grayB,'MetricThreshold',500);
 [featuresB,pointsB]=extractFeatures(grayB,pointsB);
 %%matchfeatures
-indexPairs=matchFeatures(featuresA,featuresB,'Method','Approximate','Unique',true);
-matchedPointsA=pointsA(indexPairs(:,1),:);
-matchedPointsB=pointsB(indexPairs(:,2),:);
-%figure; showMatchedFeatures(imgA,imgB,matchedPointsA,matchedPointsB);
-%%rotate the image A to the same position as B
-[tform,inlierIdx] = estimateGeometricTransform2D(matchedPointsA,matchedPointsB,'similar');
+while length(find(inlierIdx))<4
+    indexPairs = matchFeatures(featuresA,featuresB,'Method','Approximate','Unique',true);
+    matchedPointsA=pointsA(indexPairs(:,1),:);
+    matchedPointsB=pointsB(indexPairs(:,2),:);
+    
+    %%rotate the image A to the same position as B
+    
+    [tform,inlierIdx] = estimateGeometricTransform2D(matchedPointsA,matchedPointsB,'similar','Confidence',90);
+    %[F,inlierIdx] = estimateFundamentalMatrix(matchedPointsA,matchedPointsB,'Method','RANSAC',...
+    %'NumTrials',2000,'DistanceThreshold',3);
+    % if MetricThreshold==0
+    %     MetricThreshold=2000;
+    % end
+    n=n+1;
+    disp(n)
+end
+
 inlierPtsB = matchedPointsB(inlierIdx,:);
 inlierPtsA = matchedPointsA(inlierIdx,:);
+figure;showMatchedFeatures(imgA,imgB,inlierPtsA,inlierPtsB,'montage')
+
 outputView = imref2d(size(grayB));
 imgAafter=imwarp(imgA,tform,'OutputView',outputView);
-%figure;imshow(imgAafter)
+figure;imshowpair(imgAafter,imgB)
+
+% figure;imshow(pca_kmeans(imgAafter,imgB,10))
+    
+
+
 %%show the difference between the images, rosa-image B, green-melting glaciers, white-the same part
-%RA = imref2d(size(imgAafter),0.2,0.2);
-%RB = imref2d(size(imgB),0.2,0.2);
-diff=imgAafter-imgB; 
-grayDiff=rgb2gray(diff);
-X = grayslice(grayDiff,255);
-m = double(max(X(:)));
-if mean(diff(:))>20
-    imdiff=ind2rgb(X,Colormap);
-    disp('große Änderung')
-elseif mean(diff(:))<=20 && mean(diff(:))>10
-    imdiff=ind2rgb(X,Colormaplow);
-    disp('kleine Änderung')
-elseif mean(diff(:))<=10
-    imdiff=ind2rgb(X,Colormapverylow);
-    disp('sehr kleine Änderung')
+
+% diff=abs(imgAafter-imgB);
+% grayDiff=rgb2gray(diff);
+% X = grayslice(grayDiff,255);
+% m = double(max(X(:)));
+% if mean(diff(:))>20
+%     imdiff=ind2rgb(X,Colormap);
+%     disp('große Änderung')
+% elseif mean(diff(:))<=20 && mean(diff(:))>10
+%     imdiff=ind2rgb(X,Colormaplow);
+%     disp('kleine Änderung')
+% elseif mean(diff(:))<=10
+%     imdiff=ind2rgb(X,Colormapverylow);
+%     disp('sehr kleine Änderung')
+% end
+%
+% im=uint8(imdiff*255)+imgB;
+% figure;imshow(im);
+
+%% Prefilter FIR
+function pic_pre = prefilterlowpass2d(picture, kernel)
+kernel = kernel / sum(kernel(:));% normalize
+pic_pre = convn(picture, kernel, 'same'); %%central part of the convolution, which is the same size as the image
 end
-im=uint8(imdiff*255)+imgB;
-%im=uint8(im1*255)+grayB;
-
-figure;imshow(im);
-
-%figure;imshowpair(im1,imgB,'blend');
-%figure;imshowpair(imgAafter,imgB)
-%figure;imshow(imgA)
-%figure;imshow(imgB)
-%figure;imshowpair(imgAafter,RA,imgB,RB,'Scaling','independent','Parent',axes);
-
